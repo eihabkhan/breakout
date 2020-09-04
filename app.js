ed5eccc182a5dc2a3418e071c1b3081c6d9d9496
Eihab <mastereihab@gmail.com>
2020-09-04 20:13:37 +0100

    added collision detection

---
+++ app.js
@@ -0,0 +1,191 @@
+const rulesBtn = document.getElementById("rules-btn");
+const closeBtn = document.getElementById("close-btn");
+const rules = document.getElementById("rules");
+
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d");
+
+
+const ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    size: 10,
+    speed: 4,
+    dx: 4,
+    dy: -4,
+}
+
+const paddle = {
+    x: canvas.width / 2 - 40,
+    y: canvas.height - 20,
+    w: 80,
+    h: 10,
+    speed: 10,
+    dx: 0,
+}
+
+const brick = {
+    w: 70,
+    h: 20,
+    padding: 10,
+    offsetX: 45,
+    offsetY: 60,
+    visible: true 
+}
+
+
+const brickRowCount = 9
+const brickColumnCount = 5;
+
+let score = 0;
+
+// Create Bricks
+let bricks = [];
+
+for (let i = 0; i < brickRowCount; i++) {
+    bricks[i] = []
+    for (let j = 0; j < brickColumnCount; j++) {
+        const x = i * (brick.w + brick.padding) + brick.offsetX;
+        const y = j * (brick.h + brick.padding) + brick.offsetY;
+        bricks[i][j] = {x, y, ...brick}
+    }
+}
+
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
+    ctx.fillStyle = "#FFCC63"
+    ctx.fill()
+    ctx.closePath();
+}
+
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h);
+    ctx.fillStyle = "#19C9EC"
+    ctx.fill()
+    ctx.closePath();
+}
+
+
+function drawBricks() {
+    bricks.forEach(column => {
+        column.forEach(brick => {
+                ctx.beginPath();
+                ctx.rect(brick.x, brick.y, brick.w, brick.h)
+                ctx.fillStyle = brick.visible ? "#FFCC63" : "transparent"
+                ctx.fill();
+                ctx.closePath();
+        })
+    });
+}
+
+
+function draw() {
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+
+    drawBall();
+    drawPaddle();   
+    drawScore();   
+    drawBricks(); 
+}
+
+function drawScore() {
+    ctx.font = "20px Arial";
+    ctx.fillText(`Score: ${score}`, canvas.width - 100, 30)
+
+ }
+
+ function movePaddle() {
+     paddle.x += paddle.dx;
+
+    //  Wall Detection
+    if(paddle.x + paddle.w > canvas.width) {
+        paddle.x = canvas.width - paddle.w
+    }
+
+    if(paddle.x < 0) {
+        paddle.x = 0;
+    }
+ }
+
+ function moveBall() {
+     ball.x += ball.dx;
+     ball.y += ball.dy;
+
+     // Wall Collision (Horizontal)
+     if(ball.x + ball.size > canvas.width || ball.x - ball.size < 0) {
+         ball.dx *= -1
+     }
+
+     // Wall Collision (Vertical)
+     if(ball.y + ball.size > canvas.height || ball.y - ball.size < 0) {
+        ball.dy *= -1
+    }
+
+    // Paddile Collision
+    if(
+        ball.x - ball.size > paddle.x && 
+        ball.x + ball.size < paddle.x + paddle.w && 
+        ball.y + ball.size > paddle.y
+    ) {
+        ball.dy = -ball.speed
+    } 
+
+    // Brick Collision
+    bricks.forEach(column => {
+        column.forEach(brick => {
+            if (brick.visible) {
+                if (
+                    ball.x - ball.size > brick.x && // left brick side check
+                    ball.x + ball.size < brick.x + brick.w && // right brick side check
+                    ball.y + ball.size > brick.y && // top brick side check
+                    ball.y - ball.size < brick.y + brick.h // bottom brick side check
+                ) {
+                    ball.dy *= -1;
+                    brick.visible = false
+                }
+            }
+        })
+    })
+ }
+
+function update() {
+    movePaddle();
+    moveBall();
+    
+    draw();
+    requestAnimationFrame(update);
+
+}
+
+function keyDown(e) {
+    if (e.key === "Right" || e.key === "ArrowRight") {
+        paddle.dx = paddle.speed;
+    } else if(e.key === "Left" || e.key === "ArrowLeft") {
+        paddle.dx = -paddle.speed;
+    }
+}
+function keyUp(e) {
+    if (e.key === "Right" || e.key === "ArrowRight" || e.key === "Left" || e.key === "ArrowLeft") {
+        paddle.dx = 0
+        }
+}
+
+update();
+
+addEventListener("keydown", keyDown)
+addEventListener("keyup", keyUp)
+
+// Event Listeners
+rulesBtn.addEventListener("click", () => {
+    rules.classList.add("show")
+});
+
+closeBtn.addEventListener("click", () => {
+    rules.classList.remove("show")
+})
+
+
